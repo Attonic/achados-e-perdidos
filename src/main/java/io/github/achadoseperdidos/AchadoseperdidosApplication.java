9149175d8249f156718fcfd62bc10c9e218ddf30
Antonio Cleison <a.cleisonn@gmail.com>
2026-05-04 17:52:34 -0300

    Feat: Implementa aplications para o projeto

---
+++ src/main/java/io/github/achadoseperdidos/AchadoseperdidosApplication.java
@@ -0,0 +1,13 @@
+package io.github.achadoseperdidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AchadoseperdidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AchadoseperdidosApplication.class, args);
+	}
+
+}
