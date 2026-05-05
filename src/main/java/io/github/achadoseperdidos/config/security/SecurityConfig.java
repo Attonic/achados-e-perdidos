2b3a4cd8b74f64314cf665e23f697d016355024e
Antonio Cleison <a.cleisonn@gmail.com>
2026-05-05 16:44:45 -0300

    feat: implementa classes iniciais de segurança:

--- src/main/java/io/github/achadoseperdidos/config/security/SecurityConfig.java
+++ src/main/java/io/github/achadoseperdidos/config/security/SecurityConfig.java
@@ -1,4 +1,17 @@
 package io.github.achadoseperdidos.config.security;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableMethodSecurity
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+
+
+
 }
--- src/main/java/io/github/achadoseperdidos/config/security/TokenService.java
+++ src/main/java/io/github/achadoseperdidos/config/security/TokenService.java
@@ -1,4 +1,19 @@
 package io.github.achadoseperdidos.config.security;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TokenService {
+
+    private static final String TOKEN_PREFIX = "Bearer ";
+    private static final String HEADER_STRING = "Authorization";
+    private static final String ISSUE = "achados-e-perdidos-api";
+
+    @Value("${JWT_SECRET}")
+    private String jwtSecret;
+
+    private int expirationHours;
+
+
 }
