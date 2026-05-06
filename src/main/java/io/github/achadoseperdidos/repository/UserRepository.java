bfefc2cd0047349d7c98d082d102180ff3446f48
Antonio Cleison <a.cleisonn@gmail.com>
2026-05-06 18:28:48 -0300

    feat: implementa UserRepository

--- src/main/java/io/github/achadoseperdidos/repository/UserRepository.java
+++ src/main/java/io/github/achadoseperdidos/repository/UserRepository.java
@@ -1,4 +1,21 @@
 package io.github.achadoseperdidos.repository;
 
-public interface UserRepository {
+import io.github.achadoseperdidos.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<Usuario, UUID> {
+
+    Optional<Usuario> findByEmail(String email);
+
+    Optional<Usuario> findByNome(String nome);
+
+    boolean existsByNome(String nome);
+
+    boolean existsByEmail(String email);
+    
 }
